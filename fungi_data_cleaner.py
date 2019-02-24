a54ed8235434ff8749c10ffb30ef5da4c8665380
Louis Leboa <louisll1@me.com>
2019-02-23 16:57:19 -0800

    corected

--- fungi_data_cleaner.py
+++ fungi_data_cleaner.py
@@ -9,8 +9,45 @@ fungi_data.head()
 
 #Shows the columns
 fungi_data.columns
-
+I
 print("hello")
 print("bye")
 
 print("my oh my")
+
+#%%
+msg = "Hello World"
+print(msg)
+#%%
+msg = "good bye world"
+print(msg)
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+trace0 = go.Scatter(
+    x=[1, 2, 3, 4],
+    y=[10, 15, 13, 17]
+)
+trace1 = go.Scatter(
+    x=[1, 2, 3, 4],
+    y=[16, 5, 11, 9]
+)
+data = [trace0, trace1]
+
+py.plot(data, filename = 'basic-line', auto_open=True)
+
+
+
+
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import plotly
+import plotly.graph_objs as go
+
+plotly.offline.plot({
+    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+    "layout": go.Layout(title="hello world")
+}, auto_open=True)
