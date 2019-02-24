e8828d389760dd0163e8e45dc4788c2da064f837
PhongHTo <phongt@uw.edu>
2019-02-24 15:04:33 -0800

    Fixed fungi_data.drop line spacing

--- fungi_data_cleaner.py
+++ fungi_data_cleaner.py
@@ -5,7 +5,16 @@ import pandas as pd
 fungi_data = pd.read_csv("0045302-181108115102211.csv",  delimiter = "\t")
 
 ## NOTE: Drops the columns of data that are not useful to us.
-fungi_clean = fungi_data.drop(['datasetKey', 'kingdom', 'taxonRank', 'infraspecificEpithet', 'species','rightsHolder','license', 'mediaType', 'issue','institutionCode', 'collectionCode','catalogNumber', 'recordNumber', 'identifiedBy', 'dateIdentified', 'typeStatus', 'establishmentMeans', 'lastInterpreted', 'basisOfRecord',  'elevationAccuracy', 'depth', 'depthAccuracy', 'coordinatePrecision', 'elevation',], axis = 1)
+fungi_clean = fungi_data.drop(['datasetKey', 'kingdom', 'taxonRank',
+                               'infraspecificEpithet', 'species','rightsHolder',
+                               'license', 'mediaType', 'issue','institutionCode',
+                               'collectionCode','catalogNumber', 'recordNumber',
+                               'identifiedBy', 'dateIdentified', 'typeStatus',
+                               'establishmentMeans', 'lastInterpreted',
+                               'basisOfRecord',  'elevationAccuracy', 'depth',
+                               'depthAccuracy', 'coordinatePrecision',
+                               'elevation',],
+                               axis = 1)
 
 
 fungi_clean.to_csv('fungi_clean.csv',  sep = "\t", encoding='utf-8', index=False)
