95155eb2e8447d52072e7fd995d2cfb70ab8fece
Louis Leboa <louisll1@me.com>
2019-02-24 00:09:41 -0800

    2-23 push with complete cleaner

--- fungi_data_cleaner.py
+++ fungi_data_cleaner.py
@@ -4,50 +4,8 @@ import pandas as pd
 #imports the fungi_data
 fungi_data = pd.read_csv("0045302-181108115102211.csv",  delimiter = "\t")
 
-#Shows the head
-fungi_data.head()
-
-#Shows the columns
-fungi_data.columns
-I
-print("hello")
-print("bye")
-
-print("my oh my")
-
-#%%
-msg = "Hello World"
-print(msg)
-#%%
-msg = "good bye world"
-print(msg)
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-trace0 = go.Scatter(
-    x=[1, 2, 3, 4],
-    y=[10, 15, 13, 17]
-)
-trace1 = go.Scatter(
-    x=[1, 2, 3, 4],
-    y=[16, 5, 11, 9]
-)
-data = [trace0, trace1]
-
-py.plot(data, filename = 'basic-line', auto_open=True)
+## NOTE: Drops the columns of data that are not useful to us.
+fungi_clean = fungi_data.drop(['datasetKey', 'kingdom', 'taxonRank', 'infraspecificEpithet', 'species','rightsHolder','license', 'mediaType', 'issue','institutionCode', 'collectionCode','catalogNumber', 'recordNumber', 'identifiedBy', 'dateIdentified', 'typeStatus', 'establishmentMeans', 'lastInterpreted', 'basisOfRecord',  'elevationAccuracy', 'depth', 'depthAccuracy', 'coordinatePrecision', 'elevation',], axis = 1)
 
 
-
-
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-import plotly
-import plotly.graph_objs as go
-
-plotly.offline.plot({
-    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
-    "layout": go.Layout(title="hello world")
-}, auto_open=True)
+fungi_clean.to_csv('fungi_clean.csv',  sep = "\t", encoding='utf-8', index=False)
