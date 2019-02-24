fb0e95b70f04cb4e4d617a4489c667f011c6045d
Louis Leboa <louisll1@me.com>
2019-02-23 16:30:11 -0800

    commiting from vs code

--- fungi_data_cleaner.py
+++ fungi_data_cleaner.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 
+#imports the fungi_data
 fungi_data = pd.read_csv("0045302-181108115102211.csv",  delimiter = "\t")
 
+#Shows the head
 fungi_data.head()
+
+#Shows the columns
+fungi_data.columns
+
+print("hello")
