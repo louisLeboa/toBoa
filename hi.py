2b6391dc74a09d3c7345631cebf8630cddd391be
Louis Leboa <louisll1@me.com>
2019-03-06 22:14:09 -0800

    Did a little work
    mushroom data updated and worked on integrating plotly

--- hi.py
+++ hi.py
@@ -1,5 +1,14 @@
-import plotly
+import plotly as py
 import plotly.graph_objs as go
+
 plotly.offline.plot({
     "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+    "data" = [go.Bar(
+                x=['giraffes', 'orangutans', 'monkeys'],
+                y=[20, 14, 23]
+        )]
     "layout": go.Layout(title="hello world") })
+data = [go.Bar(
+            x=['giraffes', 'orangutans', 'monkeys'],
+            y=[20, 14, 23]
+    )]
